d22795a3aada7aa0d9b12220a473ec10c387ec71
Kudzai Huni <sean2kay@gmail.com>
2018-09-20 19:17:33 +0200

    Added the Dockerfile & the script to build, push, pull & deploy the Game-App as a shippable docker container.

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Script to deploy the app
+deploy(){
+# From the remote host-machine, run the following cmd
+    docker pull s34n/game-img
+    docker run --name='game' -d -it -p 8004:8004 s34n/game-img && docker logs game -f
+}
+
+# Tag-And-Push Script to tag & push the app
+tagAndPush(){
+    docker tag game-img:latest s34n/game-img:latest
+    docker push s34n/game-img:latest
+}
+
+# Rebuild-Script to clean & build the app using the Dockerfile script
+rebuild(){
+    gradle clean
+    gradle build
+    docker build -f Dockerfile -t game-img .
+    tagAndPush
+}
+
+# Let's get rid of the pre-existing docker images on the machine.
+if [[ ! -z "$(docker container ps | grep game)" ]]; then
+    echo "Game-Service Docker Container Found"
+    docker stop game && docker rm game && docker rmi game-img
+    rebuild
+else
+    echo "Game-Service Docker Container NOT Found"
+    rebuild
+fi
+
